0a85161b7f4a8286373409326220726fba32c383
shj3497 <shj3497@gmail.com>
2021-05-23 20:54:27 +0900

    map Recap

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
 import React from 'react';
 
-function Food(props){
-  console.log(props);
-  return (
-    <div className="">
-      <h1>I Like {props.name} </h1>
-      <img src={props.picture} alt="img" />
-    </div>
-    )
-}
+// function Food(props){
+//   return (
+//     <div>
+//       <h1>I Like {props.name} </h1>
+//       <img src={props.picture} alt="img" />
+//     </div>
+//     )
+// }
 
 // 객체 안에있는 변수를 직접 지정할 수도 있다.
-// function Food({favorite}){
-//   console.log(favorite);
-//   return <h1>I Like {favorite}</h1>
-// }
+function Food({name, picture}){
+  return (
+    <div>
+      <h1>I Like {name} </h1>
+      <img src={picture} alt={name} />
+    </div>
+  )
+}
 
 const foodILike = [
   {
+    id:1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id:2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id:3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id:4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id:5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+}
+
 function App() {
   return (
     <div>
       {
-        foodILike.map(object => 
-          <Food name={object.name} picture={object.image} />
-        )
+        foodILike.map(renderFood)
       }
     </div>
   );
