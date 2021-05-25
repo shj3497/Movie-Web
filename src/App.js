e4630ca5ad483a784110bf1bcabaa6c49170ad0e
shj3497 <shj3497@gmail.com>
2021-05-25 22:21:34 +0900

    Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+
 
 class App extends Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   }
 
-  // render() 된 이후 componentDidMount()를 실행시키니까(React가)
-  // setTimeout()에서 this.setState()를 실행
+  getMovies = async () => {
+    // const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    // console.log(movies.data.data.movies);
+    // 무친... ES6문법중 하나
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+
+    this.setState({
+      isLoading: false,
+      movies: movies
+    })
+  }
+  
   componentDidMount(){
-    setTimeout(() => {
-      this.setState((current) => ({isLoading: !current.isLoading}))
-    }, 6000)
+    this.getMovies();
+    
+  }
+
+  renderMovies = () => {
+    this.state.movies.map(movie => {
+      console.log(movie);
+      return(
+        <Movie 
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary}  
+          poster={movie.medium_cover_image}
+        />
+      )
+    })
   }
 
   render() {
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
     return (
       <div>
-        {isLoading ? "Loading..." : "We are ready"}
+        {
+          isLoading ? "Loading..." : movies.map(movie => {
+            return(
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary}  
+                poster={movie.medium_cover_image}
+              />
+            )
+          })
+        }
       </div>
     );
   }
